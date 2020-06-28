22ab44bff1200861ea9bc0911baa92309b60cfcb
Shelley Brook <sbrook13@gmail.com>
2020-06-28 02:04:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+
+
+function theBeatlesPlay(a,b){
+ var beatles = []
+ for (let i=0;i<a.length;i++){
+    var newString = `${a[i]} plays ${b[i]}`
+    beatles.push(newString)
+  }
+  return beatles
+}
+ let i=0
+ var allFacts = []
+ function johnLennonFacts(facts){
+   while(i<facts.length){ 
+     var newFact = `${facts[i]}!!!`
+     allFacts.push(newFact)
+     i++
+   }
+   return allFacts
+}
+  
