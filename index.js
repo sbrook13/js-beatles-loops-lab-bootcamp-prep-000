3dd965abd1b4ba1e38ce13154b2e13c6c97fba4b
Shelley Brook <sbrook13@gmail.com>
2020-06-28 02:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,17 +9,7 @@ function theBeatlesPlay(a,b){
   }
   return beatles
 }
- let i=0
- var allFacts = []
- function johnLennonFacts(facts){
-   while(i<facts.length){ 
-     var newFact = `${facts[i]}!!!`
-     allFacts.push(newFact)
-     i++
-   }
-   return allFacts
-}
-  
+ 
   
 function iLoveTheBeatles(n){
   var newArr = []
