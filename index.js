3c5edd9b25dce7ceaa69a20eeb5ac1ef4485c7a5
Shelley Brook <sbrook13@gmail.com>
2020-06-28 02:12:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function theBeatlesPlay(a,b){
   return beatles
 }
  let i=0
- var allFacts = []
+ 
  function johnLennonFacts(facts){
+   var allFacts = []
    while(i<facts.length){ 
      var newFact = `${facts[i]}!!!`
      allFacts.push(newFact)
